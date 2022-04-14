882b178ad22fe8f19d0ceca1bab9dc873a1842b2
CaffeineProgrammer <crsph@users.noreply.github.com>
2022-04-14 14:36:37 +0200

    Added Advice text

---
+++ src/views/AdviceView.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./AdviceView.scss";
+import { GetAdvice } from "../api/AdviceAPI";
+import AdviceText from "../components/AdviceText";
+import AdviceButton from "../components/AdviceButton";
+import { ReactComponent as PatternDivider } from "../assets/pattern-divider-desktop.svg";
+
+export default function AdviceView() {
+  const [adviceId, setAdviceId] = useState(0);
+  const [adviceQuote, setAdviceQuote] = useState("");
+
+  const getAdvice = async () => {
+    const data = await GetAdvice().then((res) => res.data);
+    setAdviceId(data.slip.id);
+    setAdviceQuote(data.slip.advice);
+  };
+
+  return (
+    <div className="advice-generator-container">
+      <AdviceText id={adviceId} quote={adviceQuote} />
+
+      <PatternDivider />
+
+      <AdviceButton onClick={getAdvice} />
+    </div>
+  );
+}
