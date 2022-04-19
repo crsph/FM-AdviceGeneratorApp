551cdb26db833b8fe2b4a65d5e96ee226b5b9be2
CaffeineProgrammer <crsph@users.noreply.github.com>
2022-04-19 13:20:18 +0200

    Added two SVGs

--- src/views/AdviceView.js
+++ src/views/AdviceView.js
@@ -3,7 +3,8 @@ import "./AdviceView.scss";
 import { GetAdvice } from "../api/AdviceAPI";
 import AdviceText from "../components/AdviceText";
 import AdviceButton from "../components/AdviceButton";
-import { ReactComponent as PatternDivider } from "../assets/pattern-divider-desktop.svg";
+import { ReactComponent as PatternDividerDesktop } from "../assets/pattern-divider-desktop.svg";
+import { ReactComponent as PatternDividerMobile } from "../assets/pattern-divider-mobile.svg";
 
 export default function AdviceView() {
   const [adviceId, setAdviceId] = useState(0);
@@ -23,7 +24,9 @@ export default function AdviceView() {
     <div className="advice-generator-container">
       <AdviceText id={adviceId} quote={adviceQuote} />
 
-      <PatternDivider />
+      <PatternDividerMobile className="divider-mobile" />
+
+      <PatternDividerDesktop className="divider-desktop" />
 
       <AdviceButton onClick={getAdvice} />
     </div>
